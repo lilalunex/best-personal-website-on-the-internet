bb5c2e12a5e43609f6f29488b2a11c9a1c283018
lilalunex <alexanderscharow@icloud.com>
2025-03-24 22:51:09 +0100

    Restructure Footer

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -16,13 +16,7 @@
 
 <footer>
     <hr>
-    <div class="container mx-auto pt-8 pb-4 md:pb-8 grid grid-cols-1 md-grid-cols-4 xl-grid-cols-4 text-2xl md:text-lg">
-        <div class="text-center flex flex-col gap-4 pb-4 md:pb-0">
-            <div>
-                <button on:click={() => resetGoogleAnalytics()} class="hover:underline">Reset Google Analytics choice
-                </button>
-            </div>
-        </div>
+    <div class="container mx-auto pt-8 pb-4 md:pb-8 grid grid-cols-1 md-grid-cols-3 xl-grid-cols-3">
         <div class="text-center flex flex-col gap-4 pb-4 md:pb-0">
             <div>
                 <a href="mailto:&#108;&#117;&#110;&#101;&#120;&#64;&#108;&#105;&#108;&#97;&#108;&#117;&#110;&#101;&#120;&#46;&#100;&#101;&#118;?subject=Lilalunex Mail Me"
@@ -78,6 +72,13 @@
             </div>
         </div>
         <div class="text-center flex flex-col justify-end gap-4 pb-4 md:pb-0">
+            <div>
+                <button on:click={() => resetGoogleAnalytics()} class="hover:underline">Reset Google Analytics choice
+                </button>
+            </div>
+            <div>
+                <br>
+            </div>
             <div><a href="/footer/imprint" class="hover:underline">Imprint</a></div>
             <div><a href="/footer/privacy" class="hover:underline">Privacy Policy</a></div>
         </div>
@@ -90,7 +91,7 @@
 
 <aside id="cookies-banner" aria-labelledby="cookie-heading" class="border-3 border-golden-tan rounded-2xl show hide">
     <div class="p-4 md:p-8">
-        <h3 id="cookie-heading" class="text-center text-3xl">Google Analytics</h3>
+        <h3 id="cookie-heading" class="text-center">Google Analytics</h3>
         <div class="flex justify-between pt-8 md:pt-12 gap-4 md:gap-16">
             <div>
                 <button on:click={() => setGAConsent("true")}

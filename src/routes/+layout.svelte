58a0a3cc3eee67f94dd9d12f39aae1acc5670664
lilalunex <alexanderscharow@icloud.com>
2025-03-25 23:30:17 +0100

    satisfy sv check / rm unecessary code

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,18 +5,14 @@
     import '@unocss/reset/tailwind.css';
     import '$lib/styles/global.css';
     import '$lib/styles/breathing-title.css';
-    import { setContext } from 'svelte';
 
     const Header = HeaderComponent;
     const Footer = FooterComponent;
 
     let mainElement: HTMLElement;
-
     function toggleMainClass() {
         mainElement.classList.toggle('bg-ctrl');
     }
-
-    setContext('mainElement', mainElement);
 </script>
 
 <svelte:component this={Header} on:toggleMainClass={toggleMainClass} />

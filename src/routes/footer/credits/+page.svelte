84d0d925c753b30bdc7a5f8ecc4da6763cf7ef34
lilalunex <alexanderscharow@icloud.com>
2025-03-26 02:55:35 +0100

    forgot to add design change to footer pages hihi

--- src/routes/footer/credits/+page.svelte
+++ src/routes/footer/credits/+page.svelte
@@ -1,19 +1,20 @@
-<main>
-    <div class="bg-warm-beige dark:bg-darkreader-warm-beige">
-        <div class="container mx-auto py-8 md:py-16 px-8">
-            <h1 class="title text-center underline breathing-title text-[48px] xl:text-[64px] text-shadow-[2px_1px_0_black]">
-                Credits
-            </h1>
+<div class="dark:bg-darkreader-warm-beige text-color-ctrl">
+    <div class="container mx-auto py-8 md:py-16 px-8">
+        <h1 class="breathing-title text-center underline breathing-title text-[48px] xl:text-[64px] text-shadow-[2px_1px_0_black]">
+            Credits
+        </h1>
 
-            <div class="pt-8 text-center">
-<!--                <p class="pt-8">Thanks to Sparklykitten for helping me with the design.</p>-->
-                <p class="pt-8">Thanks to my parents for always supporting me in what I do.</p>
-                <p class="pt-8">Thanks to <a href="https://www.youtube.com/c/theprimeagen" class="underline">ThePrimeagen</a> for motivating me to be a Programmer outside of work.</p>
-                <p class="pt-8">Thanks to my clients for putting their trust in me.</p>
-                <p class="pt-8">Thanks to my (previous) employers for giving me a chance.</p>
-                <p class="pt-8">Thanks to myself for putting so much work in.<br>(Website is about breathing and taking time, and here I am working deep into every single night hehe)</p>
-            </div>
-
+        <div class="pt-8 text-center">
+            <!--                <p class="pt-8">Thanks to Sparklykitten for helping me with the design.</p>-->
+            <p class="pt-8">Thanks to my parents for always supporting me in what I do.</p>
+            <p class="pt-8">Thanks to <a href="https://www.youtube.com/c/theprimeagen"
+                                         class="underline">ThePrimeagen</a> for motivating me to be a Programmer outside
+                of work.</p>
+            <p class="pt-8">Thanks to my clients for putting their trust in me.</p>
+            <p class="pt-8">Thanks to my (previous) employers for giving me a chance.</p>
+            <p class="pt-8">Thanks to myself for putting so much work in.<br>(Website is about breathing and taking
+                time, and here I am working deep into every single night hehe)</p>
         </div>
+
     </div>
-</main>
+</div>

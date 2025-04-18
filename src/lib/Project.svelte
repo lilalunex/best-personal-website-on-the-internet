b331bd2db01eb73154a5c21b6f7c6a22d4ba9bb0
lilalunex <alexanderscharow@icloud.com>
2025-04-18 11:55:24 +0200

    Project View

---
+++ src/lib/Project.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	export let imgSrc: string = '';
+	export let imgAlt: string = '';
+	export let title: string = '';
+	export let desc: string = '';
+	export let i1: string = '';
+	export let i1c: string = '';
+	export let i2: string = '';
+	export let i2c: string = '';
+	export let linkTest: string = '';
+	export let linkUrl: string = '';
+</script>
+
+<style>
+    @media (min-width: 1280px) {
+        img {
+            height: 280px;
+        }
+    }
+
+    p {
+        min-height: 81px;
+    }
+</style>
+
+<div class="rounded-3xl rounded-xl border-4 border-golden-tan p-6">
+	<div class="flex justify-center items-center">
+		<img src="{imgSrc}" alt="{imgAlt}" class="">
+	</div>
+	<h3 class="pb-4 pt-8">{title}</h3>
+	<p>{desc}</p>
+	{#if i1 || (linkTest && linkUrl)}
+		<div class="flex items-center pt-6 pb-2 gap-4">
+			{#if i1}
+				<span class="py-2 px-4 rounded-xl" style={i1c ? `background-color: ${i1c}` : ''}>{i1}</span>
+			{/if}
+			{#if linkTest && linkUrl}
+				<a href="{linkUrl}" class="py-2 px-4 underline">{linkTest}</a>
+			{/if}
+			{#if i2}
+				<span class="py-2 px-4 rounded-xl" style={i2c ? `background-color: ${i2c}` : ''}>{i2}</span>
+			{/if}
+		</div>
+	{/if}
+
+</div>
--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,158 +1,62 @@
 <script lang="ts">
-    let isOpen = {
-        future: false,
-        present: false,
-        past: false,
-        ideas: false
-    };
-
-    const expandAll = () => {
-        isOpen = {
-            future: true,
-            present: true,
-            past: true,
-            ideas: true
-        };
-    };
-
-    const collapseAll = () => {
-        isOpen = {
-            future: false,
-            present: false,
-            past: false,
-            ideas: false
-        };
-    };
+	import Project from '$lib/Project.svelte';
 </script>
 
-<!--    <div class="bg-warm-beige dark:bg-darkreader-warm-beige">-->
 <div>
-    <div class="container mx-auto py-8 md:py-16 px-8">
-        <h1 class="title text-center underline breathing-title text-[48px] xl:text-[64px] text-shadow-[2px_1px_0_black]">
-            Projects
-        </h1>
+	<div class="container mx-auto py-8 xl:py-16 px-8">
+		<h1 class="title text-center underline breathing-title text-[48px] xl:text-[64px] text-shadow-[2px_1px_0_black]">
+			Projects
+		</h1>
 
-        <div class="flex items-center justify-center pt-8 gap-8">
-            <button class="bg-button-ctrl border-deeper-brown border-2 rounded-xl px-8 py-4 relative overflow-hidden transition-all duration-300 ease-in-out shadow-md group hover:shadow-[0px_10px_20px_rgba(0,0,0,0.3)] dark:darkreader-earthy-brown"
-                    on:click={expandAll}>
-                <span class="relative inline-block before:content-[''] before:absolute before:left-1/2 before:bottom-0 before:w-0 before:h-[2px] before:bg-button-hover-underline-1 before:opacity-100 before:transition-all before:duration-300 before:origin-center group-hover:before:left-0 group-hover:before:w-full">Expand all</span>
-            </button>
-            <button class="bg-button-ctrl border-deeper-brown border-2 rounded-xl px-8 py-4 relative overflow-hidden transition-all duration-300 ease-in-out shadow-md group hover:shadow-[0px_10px_20px_rgba(0,0,0,0.3)] dark:darkreader-earthy-brown"
-                    on:click={collapseAll}>
-                <span class="relative inline-block before:content-[''] before:absolute before:left-1/2 before:bottom-0 before:w-0 before:h-[2px] before:bg-button-hover-underline-1 before:opacity-100 before:transition-all before:duration-300 before:origin-center group-hover:before:left-0 group-hover:before:w-full">Collapse all</span>
-            </button>
-        </div>
+		<div class="pt-8 grid grid-cols-1 xl:grid-cols-3 gap-4 xl:gap-8">
 
-        <div class="my-8 md:my-16 border-2 border-button-ctrl-2 rounded-xl text-color-ctrl dark:text-darkreader-earthy-brown">
-            <h2>
-                <button class="relative cursor-pointer text-2xl pl-8 py-4 w-full text-left flex justify-between items-center"
-                        on:click={() => isOpen.future = !isOpen.future}>
-                    Future
-                    <span class={`absolute right-4 bottom-3.5 text-5xl transition-transform duration-300 transform ${isOpen.future ? 'rotate-45' : ''}`}>+</span>
-                </button>
-            </h2>
+			<Project imgSrc="projects/lnxevo.png" imgAlt="Lunex Evolution" title="Lunex Evolution"
+					 desc="My WordPress Services. Never went live because I learned again, that I am not a guy for being self employed."
+					 i1="React" i1c="#57C4DC" i2="IBM Carbon" i2c="#97C1FF"
+			/>
 
-            <div class={`overflow-hidden transition-all duration-300 ease-in-out ${isOpen.future ? "max-h-100 opacity-100" : "max-h-0 opacity-0"}`}>
-                <div class="ml-8 pb-8">
-                    <p class="pt-4"><span class="fw-bold">[ ]</span> The Coworking Hub</p>
-                    <p class="pt-4"><span class="fw-bold">[ ]</span> <span class="italic">Make this page better looking...</span>
-                    </p>
-                </div>
-            </div>
-        </div>
+			<Project imgSrc="lotus.png" imgAlt="Casino Simulation" title="Casino Simulation"
+					 desc="Started to deepen PHP knowledge. Had a lod of ideas what to do next with it. Next step would be making this an actual game."
+					 i1="PHP" i1c="#4F5B93" linkTest="Play here"
+					 linkUrl="https://lilalunex.dev/casino-simulation/index.php"
+			/>
 
-        <div class="my-8 md:my-16 border-2 border-button-ctrl-2 rounded-xl text-color-ctrl dark:text-darkreader-earthy-brown">
-            <h2>
-                <button class="relative cursor-pointer text-2xl pl-8 py-4 w-full text-left flex justify-between items-center"
-                        on:click={() => isOpen.present = !isOpen.present}>
-                    Present<span
-                        class={`absolute right-4 bottom-3.5 text-5xl transition-transform duration-300 transform ${isOpen.present ? 'rotate-45' : ''}`}>+</span>
-                </button>
-            </h2>
+			<Project imgSrc="one-handed-mobile-menu/three.png" imgAlt="One handed mobile menu"
+					 title="One handed mobile menu"
+					 desc="I am a very aware when I use technology. I disliked a lot the way we navigate through mobile menus. So I came up with this."
+					 i1="Idea" i1c="#DD00DD" linkTest="Read more" linkUrl="/projects/one-handed-mobile-menu"
+			/>
 
-            <div class={`overflow-hidden transition-all duration-300 ease-in-out ${isOpen.present ? "max-h-100 opacity-100" : "max-h-0 opacity-0"}`}
-                 aria-expanded={isOpen.present}>
-                <div class="ml-8 pb-8">
-                    <p class="pt-4">
-                        <span class="fw-bold">[ ]</span> Casino Simulation - <a
-                            href="https://lilalunex.dev/casino-simulation/index.php"
-                            class="color-project-link-ctrl dark:color-darkreader-earthy-brown hover:underline underline">Playable
-                        here</a> - Github
-                        Repository:<a href="https://github.com/lilalunex/casino-simulation/"
-                                      class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">This
-                        Tab</a><a
-                            href="https://github.com/lilalunex/casino-simulation/"
-                            class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">New
-                        Tab</a>
-                    </p>
-                    <p class="pt-4">
-                        <span class="fw-bold">[ ]</span> This website you currently on - Github
-                        Repository:<a href="https://github.com/lilalunex/best-personal-website-on-the-internet/"
-                                      class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">This
-                        Tab</a><a
-                            href="https://github.com/lilalunex/best-personal-website-on-the-internet/"
-                            class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">New
-                        Tab</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+			<Project imgSrc="lilalunex_2024/desktop.png" imgAlt="2024 Website" title="2024 Website"
+					 desc="I liked this one. Turned out to be pretty good looking on mobile. I didn't put much time into that."
+					 i1="Remix (React)" i1c="#719ACC" linkTest="Read more" linkUrl="/projects/lilalunex_2024"
+			/>
 
-        <div class="my-8 md:my-16 border-2 border-button-ctrl-2 rounded-xl text-color-ctrl dark:text-darkreader-earthy-brown">
-            <h2>
-                <button class="relative cursor-pointer text-2xl pl-8 py-4 w-full text-left flex justify-between items-center"
-                        on:click={() => isOpen.past = !isOpen.past}>
-                    Past
-                    <span class={`absolute right-4 bottom-3.5 text-5xl transition-transform duration-300 transform ${isOpen.past ? 'rotate-45' : ''}`}>+</span>
-                </button>
-            </h2>
+			<Project imgSrc="projects/website2023.png" imgAlt="2023 Website" title="2023 Website"
+					 desc="Started back then to learn VueJS."
+					 i1="Vue" i1c="#42B883" linkTest="Visit here"
+					 linkUrl="https://lilalunex.dev/first-vue-website/index.html"
+			/>
 
-            <div class={`overflow-hidden transition-all duration-300 ease-in-out ${isOpen.past ? "max-h-100 opacity-100" : "max-h-0 opacity-0"}`}>
-                <div class="ml-8 pb-8">
-                    <p class="pt-4"><span class="fw-bold">[X]</span> Idea: One handed mobile menu:
-                        <a href="/projects/one-handed-mobile-menu"
-                           class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">Show
-                            me</a>
-                    </p>
-                    <p class="pt-4"><span class="fw-bold">[X]</span> 2024 Website - Used learning Remix (React
-                        Framework) -
-                        <a href="/projects/lilalunex_2024"
-                           class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">Show
-                            me</a>
-                    </p>
-                    <p class="pt-4"><span class="fw-bold">[X]</span> 2023 Website - Used learning VueJS -
-                        <a href="https://lilalunex.dev/first-vue-website/index.html"
-                           class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">This
-                            Tab</a>
-                        <a href="https://lilalunex.dev/first-vue-website/index.html" target="_blank"
-                           class="color-project-link-ctrl dark:color-darkreader-earthy-brown ml-4 hover:underline underline">New
-                            Tab</a>
-                    </p>
-                </div>
-            </div>
-        </div>
+		</div>
 
-        <div class="my-8 border-2 border-button-ctrl-2 rounded-xl text-color-ctrl dark:text-darkreader-earthy-brown">
-            <h2>
-                <button class="relative cursor-pointer text-2xl pl-8 py-4 w-full text-left flex justify-between items-center"
-                        on:click={() => isOpen.ideas = !isOpen.ideas}>
-                    Ideas
-                    <span class={`absolute right-4 bottom-3.5 text-5xl transition-transform duration-300 transform ${isOpen.ideas ? 'rotate-45' : ''}`}>+</span>
-                </button>
-            </h2>
+		<h2 class="pt-16 breathing-title underline color-golden-tan text-[32px] xl:text-[48px] text-shadow-[2px_1px_0_black] text-center">
+			Ideas
+		</h2>
 
-            <div class={`overflow-hidden transition-all duration-300 ease-in-out ${isOpen.ideas ? "max-h-100 opacity-100" : "max-h-0 opacity-0"}`}>
-                <div class="ml-8 pb-8">
-                    <p class="pt-4">- GUI for Casino Simulation -> release as actual game
-                        (Go directly all in and it with Rust...?)</p>
-                    <p class="pt-4">- Socialize Browser MMO - Idea come from to have something to
-                        play with your friends at the evenning. A game where you can just hop in without much this
-                        and
-                        that.</p>
-                    <p class="pt-4">- Nexx <span class="italic">(Psst... super secret)</span></p>
-                    <p class="pt-4">- Image Repeater</p>
-                </div>
-            </div>
-        </div>
-    </div>
+		<div class="pt-8 grid grid-cols-1 xl:grid-cols-3 gap-4 xl:gap-8">
+
+			<Project imgSrc="lotus.png" imgAlt="Nexx" title="Nexx"
+					 desc="
+					 	Sharing content to all your friends / followers. Mainly as an exercise for me to create a fullstack app. But I do feel like there can something bigger to this.
+					 "
+			/>
+
+			<Project imgSrc="lotus.png" imgAlt="The Coworking Hub" title="The Coworking Hub"
+					 desc="An idea for an website about coworking. Showing the world about pomodoro timer, coworking twitch streams to find communities / body doubling and provide other similar resources."
+			/>
+
+		</div>
+
+	</div>
 </div>
